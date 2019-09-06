1fc6c49eaca9e30d44e3285d36c8b50dc3bc8025
Ryan C. Durham <ryan@stagerightlabs.com>
2019-09-05 21:57:11 -0700

    Set up google auth via socialite

---
+++ app/Http/Controllers/Auth/GoogleLoginController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use App\Tenant;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleLoginController extends Controller
+{
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        // Ask Socialite to process the OAuth callback
+        $oauth = Socialite::driver('google')->user();
+
+        // Does this user account exist?
+        if ($user = User::where('email', $oauth->user['email'])->first()) {
+
+            // Check for an updated avatar
+            $user->avatar = $oauth->avatar;
+            $user->save();
+
+            // Initiate the user session
+            return $this->loginUserAndRedirect($user);
+        }
+
+        // The user account does not exist
+
+        // Fetch the tenant
+        $tenant = Tenant::firstOrCreate(['name' => $oauth->user['hd']]);
+
+        // Create the user
+        $user = User::create([
+            'name' => $oauth->name,
+            'email' => $oauth->user['email'],
+            'avatar' => $oauth->avatar,
+            'tenant_id' => $tenant->id,
+        ]);
+
+        // Initiate the user session
+        return $this->loginUserAndRedirect($user);
+    }
+
+    /**
+     * Initiate a user session and redirect to the dashboard.
+     *
+     * @param User $user
+     * @return \Illuminate\Http\Response
+     */
+    protected function loginUserAndRedirect(User $user)
+    {
+        Auth::login($user);
+
+        return redirect()->route('home');
+    }
+}
---
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LogoutController extends Controller
+{
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('welcome');
+    }
+}
