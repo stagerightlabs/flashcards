16a919c2338063a7a5097f92a68f2e0cc8087b16
Ryan C. Durham <ryan@stagerightlabs.com>
2019-09-21 21:20:26 -0700

    Style the welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,16 @@
 @extends('layout')
 
 @section('content')
-<h1 class="text-4xl">Welcome</h1>
-<p>Please <a href="{{ route('auth.google') }}">log in</a>.
+<div class="fixed inset-0 w-full h-screen flex items-center justify-center">
+  <div class="w-full sm:w-64 bg-gray-100 shadow-xl rounded-sm p-8 m-4">
+    <div class="flex flex-col items-center">
+      <aside class="text-gray-500 mb-8">
+          @svg('document', 'w-32 rotate-260')
+      </aside>
+      <article class="">
+        <a href="{{ route('auth.google') }}" class="bg-gray-600 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded">Enter</a>
+      </article>
+    </div>
+  </div>
+</div>
 @stop
