cb0a9def7e9abc6d8556bc6cd2bfd73096d490d6
Ryan C. Durham <ryan@stagerightlabs.com>
2019-09-27 21:34:39 -0700

    Clean up cached testing config when the test suite is run.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public static function tearDownAfterClass() : void
+    {
+        $path = base_path('bootstrap/cache/config.php');
+        if (file_exists($path)) {
+            unlink($path);
+        }
+    }
 }
